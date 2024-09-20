9ec815a5ef95b5944d8ee4e89d4bdebd01206002
bruno.aiolfi154@gmail.com <you@example.com>
2024-09-20 15:54:59 -0300

    adding navigate after gameStart

--- app/src/main/java/com/example/akinator/MainActivity.kt
+++ app/src/main/java/com/example/akinator/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.akinator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.akinator.Modules.Akinator.Models.QuestionModel
 import com.example.akinator.Modules.Akinator.Models.SessionModel
 import com.example.akinator.Modules.Akinator.Services.AkinatorService
@@ -12,7 +14,9 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.akinator.Modules.Akinator.Views.Questions.QuestionsActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -28,9 +32,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         binding.btnStart.setOnClickListener(this);
 
+        observers();
+    }
+
+    fun observers() {
+        viewModel.toastMessage.observe(this, Observer { message ->
+            message?.let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        viewModel.navigateToSecondActivity.observe(this, Observer { shouldNavigate ->
+            if (shouldNavigate == true) {
+                val intent = Intent(this, QuestionsActivity::class.java)
+                startActivity(intent)
+                viewModel.navigationComplete()  // Reset the value after navigating
+            }
+        })
     }
 
     override fun onClick(v: View?) {
-        viewModel.startGame();
+        if (v?.id == R.id.btn_start) {
+            return viewModel.startGame();
+        }
     }
 }
--- app/src/main/java/com/example/akinator/MainActivityViewModel.kt
+++ app/src/main/java/com/example/akinator/MainActivityViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.akinator
 
+import android.content.Intent
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.akinator.Modules.Akinator.Models.SessionModel
 import com.example.akinator.Modules.Akinator.Services.AkinatorService
@@ -7,19 +11,42 @@ import com.example.akinator.infra.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.activity.viewModels
 
 class MainActivityViewModel : ViewModel() {
     private val remote = RetrofitClient.createService(AkinatorService::class.java);
+
+    // LiveData to notify the UI about toast messages
+    private val _toastMessage = MutableLiveData<String>()
+    val toastMessage: LiveData<String> = _toastMessage
+
+    private val _navigateToSecondActivity = MutableLiveData<Boolean>()
+    val navigateToSecondActivity: LiveData<Boolean> get() = _navigateToSecondActivity
+
+    fun navigate() {
+        _navigateToSecondActivity.value = true
+    }
+
+    fun navigationComplete() {
+        _navigateToSecondActivity.value = false
+    }
 
     public fun startGame() {
         val call: Call<SessionModel> = remote.start()
         call.enqueue(object : Callback<SessionModel> {
             override fun onResponse(call: Call<SessionModel>, response: Response<SessionModel>) {
-                val session = response.body()
+                if (!response.isSuccessful) {
+                    // Update LiveData when there's an error
+                    _toastMessage.value = "Erro ao iniciar o jogo!"
+                    return;
+                }
+
+                navigate()
             }
 
             override fun onFailure(call: Call<SessionModel>, t: Throwable) {
-                println("Error: ${t.message}")
+                // Update LiveData when there's an error
+                _toastMessage.value = "Erro ao iniciar o jogo!"
             }
         })
     }
