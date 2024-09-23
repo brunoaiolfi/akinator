7d2acaad0af8f3b2a7d1b36270398922f6e2114d
bruno.aiolfi154@gmail.com <you@example.com>
2024-09-23 15:11:47 -0300

    Editing question by session

--- app/src/main/java/com/example/akinator/Modules/Akinator/Views/Questions/QuestionsActivity.kt
+++ app/src/main/java/com/example/akinator/Modules/Akinator/Views/Questions/QuestionsActivity.kt
@@ -2,23 +2,46 @@ package com.example.akinator.Modules.Akinator.Views.Questions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.akinator.Modules.Akinator.Entities.SessionEntity
-import com.example.akinator.R
+import com.example.akinator.databinding.ActivityQuestionsBinding
 
 class QuestionsActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: QuestionsViewModel;
+    private lateinit var binding: ActivityQuestionsBinding
+    private lateinit var viewModel: QuestionsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel = ViewModelProvider(this).get(QuestionsViewModel::class.java);
-        val tempSession = intent.getSerializableExtra("session") as SessionEntity?;
+        // Usando ViewBinding para inflar o layout
+        binding = ActivityQuestionsBinding.inflate(layoutInflater)
 
+        // Configurando o ViewModel
+        viewModel = ViewModelProvider(this).get(QuestionsViewModel::class.java)
+
+        // Obtendo o objeto session enviado pela Intent
+        val tempSession = intent.getSerializableExtra("session") as SessionEntity?
+
+        // Se o session foi passado, set no ViewModel
         tempSession?.let {
-            viewModel.setSession(it);
+            viewModel.setSession(it)
         }
 
-        setContentView(R.layout.activity_questions)
+        // Definir a view principal da activity como o root do binding
+        setContentView(binding.root)
+
+        // Configurar os observadores
+        observers()
+    }
+
+    fun observers() {
+        viewModel.session.observe(this, Observer { session ->
+            session?.let {
+                // Atualizar o texto da questão quando o LiveData mudar
+                binding.txtQuestion.text = it.question.questionText
+            }
+        })
     }
 }
